9a668b2a88200ad6df94ef7022d43497c96de719
a6037220 <a6037220@163.com>
2023-05-12 14:23:37 +0800

    更换为web请求

--- start.js
+++ start.js
@@ -1,19 +1,45 @@
 const {upload} = require('./dist');
-const argv = require('minimist')(process.argv.slice(2));
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const credentials = { email: argv.email, pass: argv.password, recoveryemail: argv.recovery_email}
-const video1 = {
-  path: argv.path,
-  title: argv.title,
-  description: argv.description,
-  onProgress: (progress) => { console.log('progress', progress) },
-  onSuccess: (result) => console.log('result', result),
-  channelName: argv.channel_name,
-  language: argv.language,
-  // publishType: argv.publish_type,
-  gameTitleSearch: argv.category,
-  tags: argv.tags.split(','),
-  isNotForKid: argv.not_for_kid,
-  thumbnail: argv.thumbnail
+const app = express();
+
+const _upload = async (req) => {
+  const argv = req.body;
+  const credentials = { email: argv.email, pass: argv.password, recoveryemail: argv.recovery_email}
+  const video1 = {
+    path: argv.path,
+    title: argv.title,
+    description: argv.description,
+    onProgress: (progress) => { console.log('progress', progress) },
+    onSuccess: (result) => console.log('result', result),
+    channelName: argv.channel_name,
+    language: argv.language,
+    // publishType: argv.publish_type,
+    gameTitleSearch: argv.category,
+    tags: argv.tags,
+    isNotForKid: argv.not_for_kid,
+    thumbnail: argv.thumbnail
+  }
+  return await upload(credentials, [video1], { headless: false })
 }
-upload(credentials, [video1], { headless: true }).then(console.log)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/upload', async (req, res) => {
+  _upload(req).then((videoLink) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({video: videoLink}));
+  }).catch((e) => {
+    const error = new Error(e);
+    res.end(JSON.stringify({error: error.message}));
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
