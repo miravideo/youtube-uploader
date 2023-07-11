ff85a9130bd8f374794ddeec96a5c3f0822dd8e6
a6037220 <a6037220@163.com>
2023-07-11 17:40:02 +0800

    feat: 增加playlist的功能

--- src/upload.ts
+++ src/upload.ts
@@ -274,7 +274,7 @@ async function uploadVideo(videoJSON: Video, messageTransport: MessageTransport)
                 // click on playlist dropdown
                 await page.evaluate((el) => el.click(), playlist[0])
                 // click New playlist button
-                const createPlaylistXPath = "//*[normalize-space(text())='Create playlist']"
+                const createPlaylistXPath = "//*[normalize-space(text())='Create playlist'] | //*[normalize-space(text())='New playlist']"
                 const createPlaylistElements = await page.$x(createPlaylistXPath)
                 await page.evaluate((el) => el.click(), createPlaylistElements[0])
 
