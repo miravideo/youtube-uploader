da38c2fe771d88b87b440a08480fb729d9ffdef7
a6037220 <a6037220@163.com>
2023-10-08 12:19:01 +0800

    feat: 切换至已经打开的浏览器

--- src/upload.ts
+++ src/upload.ts
@@ -76,7 +76,7 @@ export const upload = async (
             uploadedYTLink.push(link)
         }
 
-        await browser.close()
+        await page.close()
 
         return uploadedYTLink
     } catch (err) {
@@ -85,7 +85,7 @@ export const upload = async (
             // await page.screenshot({ path: `./${credentials.job_id}.png` });
         }
 
-        if (browser) await browser.close()
+        if (browser) await page.close()
 
         throw err;
     }
@@ -186,7 +186,7 @@ async function uploadVideo(videoJSON: Video, messageTransport: MessageTransport)
 
     const errorMessage = await page.evaluate(() => (document.querySelector('.error-area.style-scope.ytcp-uploads-dialog') as HTMLElement)?.innerText.trim())
     if (errorMessage) {
-        await browser.close()
+        await page.close()
         throw new Error('Youtube returned an error : ' + errorMessage)
     }
 
@@ -213,7 +213,7 @@ async function uploadVideo(videoJSON: Video, messageTransport: MessageTransport)
     const dailyUploadPromise = page.waitForXPath('//div[contains(text(),"Daily upload limit reached")]', { timeout: 0 }).then(() => 'dailyUploadReached');
     const uploadResult = await Promise.any([uploadCompletePromise, dailyUploadPromise])
     if (uploadResult === 'dailyUploadReached') {
-        browser.close();
+        page.close();
         throw new Error('Daily upload limit reached');
     }
 
@@ -223,7 +223,7 @@ async function uploadVideo(videoJSON: Video, messageTransport: MessageTransport)
         const process_start= page.waitForXPath('//*[contains(text(),"Processing up to SD")]', { hidden: true, timeout: 0 })
         await Promise.any([upload_complete, process_start, upload_complete_upper])
     } catch (e) {
-        browser.close();
+        page.close();
         throw new Error('Upload video failed');
     }
 
@@ -548,7 +548,7 @@ async function uploadVideo(videoJSON: Video, messageTransport: MessageTransport)
 
         fs.writeFileSync(`error/${job_id}.html`, htmlContent, 'utf8');
 
-        await browser.close()
+        await page.close()
         throw new Error(
             'Please make sure you set up your default video visibility correctly, you might have forgotten. More infos : https://github.com/fawazahmed0/youtube-uploader#youtube-setup'
         )
