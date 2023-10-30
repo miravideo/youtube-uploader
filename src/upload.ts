42091400878821a255a743a56952514c07653ac5
a6037220 <a6037220@163.com>
2023-10-30 13:38:01 +0800

    fix:修复playlist点击create失败的问题

--- src/upload.ts
+++ src/upload.ts
@@ -316,7 +316,7 @@ async function uploadVideo(videoJSON: Video, messageTransport: MessageTransport)
                 await playlistNameBoxes[0].type(' ' + playlistName.substring(0, 148))
                 await sleep(2000)
                 // click create & then done button
-                const createplaylistbtn = await page.$x("//*[normalize-space(text())='CREATE']")
+                const createplaylistbtn = await page.$x("//*[normalize-space(text())='Create']")
 
                 await sleep(2000)
                 await page.evaluate((el) => el.click(), createplaylistbtn[1])
